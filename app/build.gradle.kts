9ae35a0e3cce814a455cb6f72eea63d66be1490e
steefdesmet-svg <steefdesmet@gmail.com>
2026-07-24 09:33:52 +0200

    Bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "be.steefdesmet.waterfinderpro"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.1.2"
     }
 
     buildFeatures {
