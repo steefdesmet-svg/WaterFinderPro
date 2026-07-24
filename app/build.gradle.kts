bebeeeab9639fb3961dbea79a61d96a98c087c56
steefdesmet-svg <steefdesmet@gmail.com>
2026-07-24 10:26:17 +0200

    Bump WaterFinder Pro to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "be.steefdesmet.waterfinderpro"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.2"
+        versionCode = 3
+        versionName = "0.1.3"
     }
 
     buildFeatures {
